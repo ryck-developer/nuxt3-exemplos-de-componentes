dba62eb2245d81f1cbbe36763feab9e0db43bb98
ryck-developer <88977217+ryck-developer@users.noreply.github.com>
2023-01-11 17:11:15 -0300

    componente alerta adicionado

---
+++ components/general/alert.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="alert" :class="type">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'alert',
+  props: ['message', 'type'],
+};
+</script>
