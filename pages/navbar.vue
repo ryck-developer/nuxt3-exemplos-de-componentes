9518edfcc171fcb584329cbe571124299f052ffb
ryck-developer <88977217+ryck-developer@users.noreply.github.com>
2023-01-13 12:44:50 -0300

    1 file added

---
+++ pages/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a :href="url"><img :src="logo" alt=""></img></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: ['message', 'type'],
+  data() {
+    return {
+      texto: 'ola mundo',
+    };
+  },
+};
+</script>
