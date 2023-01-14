8f6f3a4acd2d919d88c057aeff04ef41da8c64f4
ryck-developer <88977217+ryck-developer@users.noreply.github.com>
2023-01-14 11:21:01 -0300

    1 file modified

--- pages/teste.vue
+++ pages/teste.vue
@@ -11,15 +11,5 @@ export default {
   components: {
     Navbar,
   },
-  data() {
-    return {
-      header: {
-        logo: {
-          link: 'Bem-Vindo ao site, agora que você é um colaborador, que tal dar uma olhada na sessão de aprendizado?',
-          imagem: 'information',
-        },
-      },
-    };
-  },
 };
 </script>
