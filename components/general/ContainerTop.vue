77ed1a3a27c852ff1be3222220a38850adb02717
ryck-developer <88977217+ryck-developer@users.noreply.github.com>
2023-01-14 23:39:29 -0300

    grid layout adicionado

---
+++ components/general/ContainerTop.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="item-1" style="background-color: green">Conteúdo do item 1</div>
+    <div class="item-2" style="background-color: red">Conteúdo do item 2</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContainerTop',
+};
+</script>
+
+<style>
+.container {
+  display: grid;
+  grid-template-columns: 60% 40%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <Navbar />
+    <ContainerTop />
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/general/Navbar.vue';
+import ContainerTop from '~/components/general/ContainerTop.vue';
 
 export default {
   components: {
     Navbar,
+    ContainerTop,
   },
 };
 </script>
