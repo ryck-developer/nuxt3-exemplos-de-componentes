d8db03a30b9fb1b133990a11650fbb59671e0905
ryck-developer <88977217+ryck-developer@users.noreply.github.com>
2023-01-16 13:59:14 -0300

    2 files added

---
+++ components/general/HookOne.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="HookOne">
+    <button @click="subtrair">-</button>
+    <span>{{ numero }}</span>
+    <button @click="adicionar">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HookOne',
+  data() {
+    return {
+      numero: 10,
+    };
+  },
+  methods: {
+    adicionar() {
+      this.numero++;
+    },
+    subtrair() {
+      this.numero--;
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  padding: 10px 20px;
+  margin: 10px;
+}
+.HookOne {
+  display: flex;
+  border: 0px solid;
+  border-radius: 10px;
+  background: #f0f0f0;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ pages/ReactHooks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <HookOne />
+  </div>
+</template>
+
+<script>
+import HookOne from '~/components/general/HookOne.vue';
+
+export default {
+  components: {
+    HookOne,
+  },
+};
+</script>
